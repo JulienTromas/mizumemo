fe296c9b93f744daa266b26adb8c2c58e25ad792
Julien Tromas <56099272+JulienTromas@users.noreply.github.com>
2021-01-05 12:08:44 +0900

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,22 +8,22 @@ export default function Navbar(props) {
 
   return (
     <>      
-<div>
-  <div id="navBar" className="navbar">
-    <NavLink to="/Home">
-      <button className="navbar-header">Home</button>
-    </NavLink>
-    <NavLink to="/Select">
-      <button id="select-plant" className="navbar-header">Select a plant</button>
-    </NavLink>
-    <NavLink to="/Collection">
-            <button
-            id="plant-collection"
-            className="navbar-header">
-            My collection
-            </button>
-        </NavLink>
-      </div>
+      <div>
+        <div id="navBar" className="navbar">
+          <NavLink to="/Home">
+            <button className="navbar-header">Home</button>
+          </NavLink>
+          <NavLink to="/Select">
+            <button id="select-plant" className="navbar-header">Select a plant</button>
+          </NavLink>
+          <NavLink to="/Collection">
+                  <button
+                  id="plant-collection"
+                  className="navbar-header">
+                  My collection
+                  </button>
+              </NavLink>
+            </div>
       </div>
     </>
   );
