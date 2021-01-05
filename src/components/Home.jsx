93031fc35265e83030cf8f7756783bdfdae8109b
Julien Tromas <56099272+JulienTromas@users.noreply.github.com>
2021-01-05 12:08:08 +0900

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,8 +4,7 @@ export default function Home() {
     return (
         <div class="text-center">
             <h1>Welcome to MizuMemo!</h1>
-<h4>Please, choose a plant to add to your collection</h4>
-            
+            <h4>Please, choose a plant to add to your collection</h4>         
         </div>
     )
 }
